7d43f7a2c6cff006918b66ff4d6a7c6cb92d1f6b
ironrabb <gimmyeongil641@gmail.com>
2026-09-23 11:33:18 +0900

    MIT TASK M

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+// MIT TASK M
+function getSquareNumbers(arr: number[]) {
+  return arr.map((num: number) => ({
+    raqam: num,
+    kvadrati: num * num,
+  }));
+}
+
+// ===== TEST =====
+console.log(getSquareNumbers([1, 2, 3]));
+// [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]

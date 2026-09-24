4faade381e354d60d7fe4298c180b6cc55b46c49
ironrabb <gimmyeongil641@gmail.com>
2026-09-25 00:16:40 +0900

    routerlar and member schema

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import path from "path";
+import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -14,5 +16,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
+app.use("/admin", routerAdmin);
+app.use("/", router);
 
 export default app;
+
+// EJS
+// REACT

03d58ecbddae7d2c6a81670545a18bd2a4e19d1b
ironrabb <gimmyeongil641@gmail.com>
2026-09-25 00:17:36 +0900

    MIT TASK N

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,22 @@
-// MIT TASK M
-function getSquareNumbers(arr: number[]) {
-  return arr.map((num: number) => ({
-    raqam: num,
-    kvadrati: num * num,
-  }));
+// N TASK
+
+function palindromCheck(input: string): boolean {
+  const reverseInput = input.toLowerCase().split("").reverse().join("");
+  if (input.toLowerCase() == reverseInput) return true;
+
+  return false;
 }
 
-// ===== TEST =====
-console.log(getSquareNumbers([1, 2, 3]));
-// [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]
+console.log(palindromCheck("mom"));
+console.log(palindromCheck("non"));
+// // MIT TASK M
+// function getSquareNumbers(arr: number[]) {
+//   return arr.map((num: number) => ({
+//     raqam: num,
+//     kvadrati: num * num,
+//   }));
+// }
+
+// // ===== TEST =====
+// console.log(getSquareNumbers([1, 2, 3]));
+// // [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]
